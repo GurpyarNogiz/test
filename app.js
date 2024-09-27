184167e72b03478d5117c2f91f47adb89e46ec17
GurpyarNogiz <gurpyar@nogiz.com>
2024-09-27 18:08:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to simulate some processing
+app.use((req, res, next) => {
+    console.log(`Received request: ${req.method} ${req.url}`);
+    setTimeout(next, 5000); // Simulate delay
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, world! Current tioioiime : ' + new Date().toLocaleString());
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
