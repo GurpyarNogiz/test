e1990e19016a1cf0073923861def29212fc20f10
GurpyarNogiz <gurpyar@nogiz.com>
2024-11-11 15:52:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ const port = 3000;
 
 
 // Middleware to simulate some processing
-app.use((req, res, next) => {
-    console.log(`Received request: ${req.method} ${req.url}`);
-    setTimeout(next, 5000); // Simulate delay
-});
-///
-app.get('/', (req, res) => {
-    res.send('workingCurrent tioioiime : ' + new Date().toLocaleString());
-});
+console.log("runninga aoooooooooooooooooooooooooooooooooo")
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
